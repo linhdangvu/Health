52adb7ad405a2029ba78253df761b15b9ce4ff9b
Linh DANG VU <linhdangvu@MacBook-Pro-de-Linh.local>
2023-02-03 09:10:47 +0100

    Done v1

--- src/components/Health.vue
+++ src/components/Health.vue
@@ -11,7 +11,9 @@
   } from "vue";
   import ApexChart from "vue3-apexcharts";
   import { useFile } from "../stores/useFile";
+  import { toApexChart, toApexChart2 } from "../utils/apexchart";
   import sleep from "../utils/sleep";
+  import { addArray, clearArray } from "../utils/useArray";
 
   /* PROPS */
   const props = defineProps<{
@@ -31,6 +33,7 @@
   const moyenneProduit = ref(0);
   const moyenneProdTop1 = ref(0);
   const moyenneProdTop2 = ref(0);
+  const loadingChart = ref(false);
   let topMagasinByFab: any[] = [];
   let topMagasinByProd: any[] = [];
   const fabData: any[] = [];
@@ -38,9 +41,25 @@
   const magData: any[] = [];
   const prodByFab: any[] = [];
   let topMagasin: any = {};
-
-  const fabIDO = 965;
+  const dateData: any[] = [];
+  const chartTopFab = reactive<any[]>([]);
+  const checkFabID = ref(965);
   const oldCategory = ref(props.catID);
+  const dateString = [
+    "Janvier 2022",
+    "Février 2022",
+    "Mars 2022",
+    "Avril 2022",
+    "Mai 2022",
+    "Juin 2022",
+    "Juillet 2022",
+    "Août 2022",
+    "Septembre 2022",
+    "Octobre 2022",
+    "Novembre 2022",
+    "Decembre 2022",
+    "Janvier 2023",
+  ];
 
   const fabricantSorted = (data: any) => {
     if (!data) return [];
@@ -112,8 +131,14 @@
           }
         }
       }
-    });
 
+      if (!dateData.includes(Number(item.dateID.slice(0, 6)))) {
+        dateData.push(Number(item.dateID.slice(0, 6)));
+      }
+
+      dateData.sort((a: any, b: any) => a - b);
+    });
+    console.log(dateData);
     // Question 1.1
     nombreFab.value = fabData.length;
     nombreProd.value = prodData.length;
@@ -158,34 +183,60 @@
       console.log(topMagasin);
       // Question 1.4
       // Pose fabID = 506, catID = 5, magasin top 10
-      const fabIDO = 965;
 
-      const list10MagFab = topMagasinByFab.map((ele: any) => {
-        return Object.keys(ele).pop();
-      });
-      const list10MagProd = topMagasinByProd.map((ele: any) => {
-        return Object.keys(ele).pop();
-      });
+      handleFab(checkFabID.value, category, topMagasinByFab, topMagasinByProd);
+    }
+  };
 
-      const filteredProductByFabTop: any[] = [];
-      const filteredProductByProdTop: any[] = [];
+  const handleFab = (
+    fab: number,
+    category: number,
+    topMagasinByFab: any[],
+    topMagasinByProd: any[]
+  ) => {
+    console.log("Start Fab", topMagasinByFab, topMagasinByProd);
+    // Question 1.4
+    // Pose fabID = 506, catID = 5, magasin top 10
+    const list10MagFab = topMagasinByFab.map((ele: any) => {
+      return Object.keys(ele).pop();
+    });
+    const list10MagProd = topMagasinByProd.map((ele: any) => {
+      return Object.keys(ele).pop();
+    });
 
-      props.data.forEach((item: any) => {
+    const filteredProductByFabTop: any[] = [];
+    const filteredProductByProdTop: any[] = [];
+    const chartFab: any[] = [];
+    console.log(props.data.length);
+    props.data.forEach((item: any) => {
+      if (
+        Number(item.catID) === Number(category) &&
+        Number(item.fabID) === Number(checkFabID.value)
+      ) {
         if (
-          Number(item.catID) === category &&
-          Number(item.fabID) === fabIDO &&
-          list10MagFab.includes(item.magID) &&
-          !filteredProductByFabTop.includes(item.prodID)
+          !filteredProductByFabTop.includes(item.prodID) &&
+          list10MagFab.includes(item.magID)
         ) {
           filteredProductByFabTop.push(item.prodID);
-          // console.log("FAB", item);
+          console.log("FAB", item);
+
+          // Janvier
+          dateData.forEach((i2: any, index: number) => {
+            if (!chartFab[index]) chartFab[index] = 0;
+            if (Number(item.dateID.slice(0, 6)) === Number(i2)) {
+              if (chartFab[index] !== 0) {
+                const n = chartFab[index];
+                chartFab[index] = (n + 1 / 10) * 100;
+              } else {
+                chartFab[index] = (1 / 10) * 100;
+              }
+            }
+          });
         }
 
         if (
-          Number(item.catID) === category &&
-          Number(item.fabID) === fabIDO &&
-          list10MagProd.includes(item.magID) &&
-          !filteredProductByProdTop.includes(item.prodID)
+          !filteredProductByProdTop.includes(item.prodID) &&
+          list10MagProd.includes(item.magID)
         ) {
           filteredProductByProdTop.push(item.prodID);
           // console.log("PROD", item);
@@ -193,13 +244,26 @@
         // if (Number(item.catID) === props.catID && Number(item.magID) === 22) {
         //   console.log(item.fabID);
         // }
-      });
+      }
+    });
+    console.log(chartFab);
+    clearArray(chartTopFab);
+    addArray(chartTopFab, chartFab);
+    // console.log(filteredProductByFabTop, filteredProductByProdTop);
+    moyenneProdTop1.value = (filteredProductByFabTop.length / 10) * 100;
+    moyenneProdTop2.value = (filteredProductByProdTop.length / 10) * 100;
 
-      // console.log(filteredProductByFabTop, filteredProductByProdTop);
-      moyenneProdTop1.value = (filteredProductByFabTop.length / 10) * 100;
-      moyenneProdTop2.value = (filteredProductByProdTop.length / 10) * 100;
-    }
+    console.log("End Fab");
   };
+
+  const handleCount = async () => {
+    loadingChart.value = true;
+    await sleep(1);
+    handleFab(checkFabID.value, props.catID, topMagasinByFab, topMagasinByProd);
+    await sleep(1);
+    loadingChart.value = false;
+  };
+
   onMounted(() => {
     isLoading.value = true;
     console.log("Loading...");
@@ -211,13 +275,6 @@
       console.log(e);
     }
   });
-
-  const toApexChart = (data: any[]) => {
-    return data.map((item: any) => {
-      const key: any = Object.keys(item).pop();
-      return { x: key, y: item[key] };
-    });
-  };
 
   const optionsFab = computed(() => {
     if (!isLoading.value) {
@@ -269,7 +326,7 @@
           type: "radialBar",
         },
         series: [moyenneProdTop1.value],
-        labels: ["Moyenne"],
+        labels: ["Fabricants"],
       };
     }
     return undefined;
@@ -283,14 +340,35 @@
           type: "radialBar",
         },
         series: [moyenneProdTop2.value],
-        labels: ["Moyenne"],
+        labels: ["Produits"],
+      };
+    }
+    return undefined;
+  });
+
+  const optionsTopFab = computed(() => {
+    if (!isLoading.value) {
+      return {
+        chart: {
+          type: "bar",
+        },
+        title: {
+          text: "Moyenne de produits par mois",
+          align: "center",
+        },
+        series: [
+          {
+            name: "Moyenne de produits",
+            data: toApexChart2(dateString, chartTopFab),
+          },
+        ],
       };
     }
     return undefined;
   });
 
   watchEffect(async () => {
-    console.log("Watch", props.catID);
+    console.log("Watch", checkFabID);
     if (Number(oldCategory.value !== Number(props.catID))) {
       console.log("Restart by category");
       oldCategory.value = Number(props.catID);
@@ -300,6 +378,10 @@
       await sleep(1);
       isLoading.value = false;
       console.log("End restart by category");
+    }
+
+    if (!isLoading.value) {
+      console.log(toApexChart2(dateData, chartTopFab));
     }
   });
 </script>
@@ -336,7 +418,7 @@
     </div>
 
     <div v-if="props.mag">
-      <h3>Top 10 magasins ont la plupart fabricants</h3>
+      <h3>Top 10 magasins</h3>
 
       <div class="box-container">
         <ApexChart
@@ -348,38 +430,80 @@
         ></ApexChart>
 
         <ApexChart
-          v-if="optionsMoyProd1 && props.mag"
-          :type="optionsMoyProd1.chart.type"
-          :height="optionsMoyProd1.chart.height"
-          :options="optionsMoyProd1"
-          :series="optionsMoyProd1.series"
-        ></ApexChart>
-      </div>
-
-      <h3>Top 10 magasins ont la plupart produits</h3>
-
-      <div class="box-container">
-        <ApexChart
           v-if="optionsProd && props.mag"
           type="bar"
           width="500"
           :options="optionsProd"
           :series="optionsProd.series"
         ></ApexChart>
+      </div>
 
-        <ApexChart
-          v-if="optionsMoyProd2 && props.mag"
-          :type="optionsMoyProd2.chart.type"
-          :height="optionsMoyProd2.chart.height"
-          :options="optionsMoyProd2"
-          :series="optionsMoyProd2.series"
-        ></ApexChart>
+      <h3>Moyenne de produits by fabricants dans top 10 magasins</h3>
+      <!-- pour calculer la moyenne de produits -->
+      <p>
+        Entrer la fabricants:
+        <input class="fab-input" type="number" v-model="checkFabID" />
+        <button class="fab-btn" @click="handleCount">Compter</button>
+      </p>
+      <div v-if="loadingChart">Loading chart ...</div>
+      <div v-else>
+        <div class="box-container">
+          <ApexChart
+            v-if="optionsMoyProd1 && props.mag"
+            :type="optionsMoyProd1.chart.type"
+            :height="optionsMoyProd1.chart.height"
+            :options="optionsMoyProd1"
+            :series="optionsMoyProd1.series"
+          ></ApexChart>
+
+          <ApexChart
+            v-if="optionsMoyProd2 && props.mag"
+            :type="optionsMoyProd2.chart.type"
+            :height="optionsMoyProd2.chart.height"
+            :options="optionsMoyProd2"
+            :series="optionsMoyProd2.series"
+          ></ApexChart>
+        </div>
+
+        <div class="box-container">
+          <ApexChart
+            v-if="optionsTopFab"
+            type="bar"
+            width="500"
+            :options="optionsTopFab"
+            :series="optionsTopFab.series"
+          ></ApexChart>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
+  .fab-input {
+    height: 20px;
+    font-size: 1rem;
+    text-align: center;
+    width: 80px;
+    padding: 5px;
+  }
+
+  .fab-btn {
+    margin-left: 10px;
+    padding: 5px;
+    color: black;
+    background-color: white;
+    border: solid 1px black;
+  }
+
+  .fab-btn:hover {
+    margin-left: 10px;
+    padding: 5px;
+    color: white;
+    background-color: black;
+    border: solid 1px black;
+  }
+
   .buttons {
     display: flex;
     flex-wrap: wrap;
--- src/components/HeathDate.vue
+++ src/components/HeathDate.vue
@@ -13,6 +13,7 @@
   } from "vue";
   import ApexChart from "vue3-apexcharts";
   import { useFile } from "../stores/useFile";
+import { toApexChart, toApexChart2 } from "../utils/apexchart";
   import sleep from "../utils/sleep";
 
   /* PROPS */
@@ -180,18 +181,7 @@
     }
   });
 
-  const toApexChart2 = (data1: any[], data2: any[]) => {
-    return data1.map((item: any, index: number) => {
-      return { x: item, y: data2[index] };
-    });
-  };
 
-  const toApexChart = (data: any[]) => {
-    return data.map((item: any) => {
-      const key: any = Object.keys(item).pop();
-      return { x: key, y: item[key] };
-    });
-  };
 
   const optionsQ21 = computed(() => {
     if (!isLoading.value) {
